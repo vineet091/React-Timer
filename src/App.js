fae9a77f5e1123b2b54d89c32852a5a003f7ae1e
Vineet Gupta <vineet091@users.noreply.github.com>
2022-11-28 17:18:21 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,122 +1,40 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import "./styles.css";
 import Timer from "./Timer";
-// timer, play, pause, reset
-// global reset, play, pause
 
 export default function App() {
-  const Timer1_ID = useRef(null);
-  const Timer2_ID = useRef(null);
-  const Timer3_ID = useRef(null);
-  const [timer1, setTimer1] = useState(60);
-  const [timer2, setTimer2] = useState(60);
-  const [timer3, setTimer3] = useState(60);
+  const [currentState, setCurrentState] = useState(1);
 
-  const startTimer = (callback2, timerId) => {
-    timerId.current = setInterval(
-      (callback1) => {
-        callback1((time) => {
-          if (time >= 1) {
-            return time - 1;
-          } else {
-            clearInterval(timerId.current);
-            return 0;
-          }
-        });
-      },
-      1000,
-      callback2
-    );
-  };
-
-  useEffect(() => {
-    startTimer(setTimer1, Timer1_ID);
-    startTimer(setTimer2, Timer2_ID);
-    startTimer(setTimer3, Timer3_ID);
-    return () => {
-      clearInterval(Timer1_ID?.current);
-      clearInterval(Timer2_ID?.current);
-      clearInterval(Timer3_ID?.current);
-    };
-  }, []);
-
-  const clearTimer = () => {};
-
-  const onPause = (timerID) => {
-    if (timerID) {
-      console.log(timerID);
-      clearInterval(timerID.current);
-    } else {
-      clearInterval(Timer1_ID?.current);
-      clearInterval(Timer2_ID?.current);
-      clearInterval(Timer3_ID?.current);
-    }
-  };
-  const onPlay = (callbackFn, timerID) => {
-    if (timerID) {
-      if (timerID?.current) {
-        console.log(timerID);
-        startTimer(callbackFn, timerID);
-      }
-    } else {
-      startTimer(setTimer1, Timer1_ID);
-      startTimer(setTimer2, Timer2_ID);
-      startTimer(setTimer3, Timer3_ID);
-    }
-  };
-  const onReset = (callbackFn, timerID) => {
-    if (timerID) {
-      // if (timerID.current) {
-      //   clearInterval(timerID.current);
-      // }
-      callbackFn(60);
-      console.log(timerID);
-      // startTimer(callbackFn, timerID);
-    } else {
-    }
-  };
   return (
     <div className="App">
       <div className="g-actions">
-        <button className="btn" type="button" onClick={() => onPlay()}>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => setCurrentState(1)}
+        >
           Play All
         </button>
         |
-        <button className="btn" type="button" onClick={() => onPause()}>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => setCurrentState(2)}
+        >
           Pause All
         </button>
         |
-        <button className="btn" type="button" onClick={() => onReset()}>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => setCurrentState(3)}
+        >
           Reset All
         </button>
       </div>
-      <Timer
-        id="1"
-        timer={timer1}
-        onPause={onPause}
-        onPlay={onPlay}
-        onReset={onReset}
-        timerID={Timer1_ID}
-        setTimer={setTimer1}
-      />
-      <Timer
-        id="2"
-        timer={timer2}
-        onPause={onPause}
-        onPlay={onPlay}
-        onReset={onReset}
-        setTimer={setTimer2}
-        timerID={Timer2_ID}
-      />
-      <Timer
-        id="3"
-        timer={timer3}
-        onPause={onPause}
-        onPlay={onPlay}
-        onReset={onReset}
-        timerID={Timer3_ID}
-        setTimer={setTimer3}
-      />
+      <Timer key={`timer-1`} id="1" currentState={currentState} />
+      <Timer key={`timer-2`} id="2" currentState={currentState} />
+      <Timer key={`timer-3`} id="3" currentState={currentState} />
     </div>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,28 +1,77 @@
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
-const Timer = ({ id, timer, onReset, onPause, onPlay, timerID, setTimer }) => {
+const Timer = ({ id, currentState }) => {
+  const Timer_ID = useRef(null);
+  const [timer, setTimer] = useState(60);
+
+  const startTimer = () => {
+    Timer_ID.current = setInterval(
+      (callback1) => {
+        callback1((time) => {
+          if (time >= 1) {
+            return time - 1;
+          } else {
+            clearInterval(Timer_ID.current);
+            Timer_ID.current = null;
+            return 0;
+          }
+        });
+      },
+      1000,
+      setTimer
+    );
+  };
+
+  useEffect(() => {
+    if (currentState === 1) {
+      startTimer();
+    }
+    return () => {
+      clearInterval(Timer_ID?.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentState === 1) {
+      onPlay();
+    }
+    if (currentState === 2) {
+      onPause();
+    }
+    if (currentState === 3) {
+      onReset();
+    }
+  }, [currentState]);
+
+  const onPause = () => {
+    clearInterval(Timer_ID.current);
+    Timer_ID.current = null;
+  };
+  const onPlay = () => {
+    if (!Timer_ID?.current) {
+      startTimer();
+    }
+  };
+
+  const onReset = () => {
+    setTimer(60);
+  };
+
   return (
     <div>
       <h2>{`Timer ${id}`}</h2>
       <div>{timer}</div>
       <div>
-        <button
-          className="btn"
-          type="button"
-          onClick={() => onPlay(setTimer, timerID)}
-        >
+        <button className="btn" type="button" onClick={() => onPlay()}>
           Play
         </button>
         |
-        <button className="btn" type="button" onClick={() => onPause(timerID)}>
+        <button className="btn" type="button" onClick={() => onPause()}>
           Pause
         </button>
         |
-        <button
-          className="btn"
-          type="button"
-          onClick={() => onReset(setTimer, timerID)}
-        >
+        <button className="btn" type="button" onClick={() => onReset()}>
           Reset
         </button>
       </div>
